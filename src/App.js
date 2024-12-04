b05c19a6d14f1fe957782e8c4845460ef9009c62
Thupakula Gowri Shankar <gowrishankarlpu@gmail.com>
2024-12-04 12:26:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './Components/common/Navbar';
+import FooterComponent from './Components/common/Footer';
+import LoginPage from './Components/auth/LoginPage';
+import RegisterPage from './Components/auth/RegisterPage';
+import HomePage from './Components/home/HomePage';
+import AllRoomsPage from './Components/booking_rooms/AllRoomsPage';
+import RoomDetailsBookingPage from './Components/booking_rooms/RoomDetailsPage';
+import FindBookingPage from './Components/booking_rooms/FindBookingPage';
+import AdminPage from './Components/admin/AdminPage';
+import ManageRoomPage from './Components/admin/ManageRoomPage';
+import EditRoomPage from './Components/admin/EditRoomPage';
+import AddRoomPage from './Components/admin/AddRoomPage';
+import ManageBookingsPage from './Components/admin/ManageBookingsPage';
+import EditBookingPage from './Components/admin/EditBookingPage';
+import ProfilePage from './Components/profile/ProfilePage';
+import EditProfilePage from './Components/profile/EditProfilePage';
+import { ProtectedRoute, AdminRoute } from './service/guard';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            
+            <Route exact path="/home" element={<HomePage />} />
+            <Route exact path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/rooms" element={<AllRoomsPage />} />
+            <Route path="/find-booking" element={<FindBookingPage />} />
+
+          
+            <Route path="/room-details-book/:roomId"
+              element={<ProtectedRoute element={<RoomDetailsBookingPage />} />}
+            />
+            <Route path="/profile"
+              element={<ProtectedRoute element={<ProfilePage />} />}
+            />
+            <Route path="/edit-profile"
+              element={<ProtectedRoute element={<EditProfilePage />} />}
+            />
+
+            {/* Admin Routes */}
+            <Route path="/admin"
+              element={<AdminRoute element={<AdminPage />} />}
+            />
+            <Route path="/admin/manage-rooms"
+              element={<AdminRoute element={<ManageRoomPage />} />}
+            />
+            <Route path="/admin/edit-room/:roomId"
+              element={<AdminRoute element={<EditRoomPage />} />}
+            />
+            <Route path="/admin/add-room"
+              element={<AdminRoute element={<AddRoomPage />} />}
+            />
+            <Route path="/admin/manage-bookings"
+              element={<AdminRoute element={<ManageBookingsPage />} />}
+            />
+            <Route path="/admin/edit-booking/:bookingCode"
+              element={<AdminRoute element={<EditBookingPage />} />}
+            />
+
+            {/* Fallback Route */}
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+        </div>
+        <FooterComponent />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/common/Footer.jsx
@@ -0,0 +1,11 @@
+const FooterComponent = ()=>{
+
+    return (
+        <footer>
+            <span className="my-footer">
+                Thupakula Hotels | All Rights Reserved &copy; {new Date().getFullYear()};
+            </span>
+        </footer>
+    )
+}
+export default FooterComponent;
